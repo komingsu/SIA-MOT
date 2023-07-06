b7b73270795ae8a453ad3bc4ec88e298ca5d19d8
Ko Minsu <rh987201@gmail.com>
2023-07-06 21:48:12 +0900

    Update EDA.py
    1.make_det_dataset 함수 변경
    1-1. 칼럼명을 coco, yolo 와 같이 박스type으로 변경
    1-2. list, list[list] type들을 읽을 때 ast.literal_eval 을 사용하여 리스트로 읽을 수 있도록 변경
    1-3. occ, trun 제거

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -27,12 +27,12 @@ def make_dataset(anno_path, seq_path):
             return 2
 
     df = pd.DataFrame(columns=[
-        "video_name", "frame_index", "shape", "target_id", "xywh", "norm_xywh",
+        "video_name", "frame_index", "shape", "target_id", "coco", "norm_coco",
         "object_size", "norm_object_size", "score", "class", "truncation",
         "occlusion"
     ])
 
-    v_name, frame, id, xywh, score, cls, trun, occ, shape, object_size, norm_xywh, norm_object_size = [],[],[],[],[],[],[],[],[],[],[],[]
+    v_name, frame, id, coco, score, cls, trun, occ, shape, object_size, norm_coco, norm_object_size = [],[],[],[],[],[],[],[],[],[],[],[]
 
     g_anno = glob(anno_path + "/*")
     g_seq = glob(seq_path + "/*")
@@ -56,28 +56,28 @@ def make_dataset(anno_path, seq_path):
                 v_name.append(name)
                 frame.append(int(line[0]))
                 id.append(int(line[1]))
-                temp_xywh = list(map(int, line[2:6]))
-                xywh.append(temp_xywh)
-                nx = temp_xywh[0] / img_shape[1]
-                ny = temp_xywh[1] / img_shape[0]
-                nw = temp_xywh[2] / img_shape[1]
-                nh = temp_xywh[3] / img_shape[0]
-                norm_xywh.append([nx, ny, nw, nh])
+                temp_coco = list(map(int, line[2:6]))
+                coco.append(temp_coco)
+                nx = temp_coco[0] / img_shape[1]
+                ny = temp_coco[1] / img_shape[0]
+                nw = temp_coco[2] / img_shape[1]
+                nh = temp_coco[3] / img_shape[0]
+                norm_coco.append([nx, ny, nw, nh])
 
                 score.append(int(line[6]))
                 cls.append(int(line[7]))
                 trun.append(int(line[8]))
                 occ.append(int(line[9]))
                 shape.append(img_shape)
-                object_size.append(temp_xywh[2] * temp_xywh[3])
+                object_size.append(temp_coco[2] * temp_coco[3])
                 norm_object_size.append(nw * nh)
 
     df["video_name"] = v_name
     df["frame_index"] = frame
     df["shape"] = shape
     df["target_id"] = id
-    df["xywh"] = xywh
-    df["norm_xywh"] = norm_xywh
+    df["coco"] = coco
+    df["norm_coco"] = norm_coco
     df["score"] = score
     df["class"] = cls
     df["truncation"] = trun
@@ -155,42 +155,65 @@ def get_sample_by_class(df, object_class, root_path, count=10, random_state=1, p
     sample_df["img_path"] = root_path + sample_df["video_name"]+sample_df["frame_index"].apply(lambda x : "/"+str(x).zfill(7) +".jpg")
     imgs = [cv2.cvtColor(cv2.imread(img_path),cv2.COLOR_RGB2BGR) for img_path in sample_df["img_path"].values]
 
-    boxes = sample_df["xywh"].values
+    boxes = sample_df["coco"].values
 
     crops = crop_box(imgs, boxes, pad=padding)
     plot_ftus(crops, object_class)
 
+def coco2xyxy(boxes):
+    def _coco2xyxy(box):
+        xmin,ymin,w,h = box
+        xmax = xmin + w
+        ymax = ymin + h
+        return [xmin, ymin, xmax, ymax]
+    return [_coco2xyxy(box) for box in boxes]
+
+def coco2yolo(boxes):
+    def _coco2yolo(box):
+        xmin,ymin,w,h = box
+        xmin = xmin + w/2
+        ymin = ymin + h/2
+        return [xmin, ymin, w, h]
+    return [_coco2yolo(box) for box in boxes]
+
 def make_detection_dataframe(data):
     video_name = []
     frame_index = []
-    img_shape = []
-    xywhs = []
-    norm_xywhs = []
-    scores = []
+    heigth = []
+    width = []
+    coco  = []
     classes = []
     unified_classes = []
-    truncations = []
-    occlustions = []
+
     for (k1,k2), group in data.groupby(['video_name',"frame_index"]):
         video_name.append(k1)
         frame_index.append(k2)
-        img_shape.append(group["shape"].values[0])
-        xywhs.append( [ast.literal_eval(i) for i in group["xywh"].values] )
-        norm_xywhs.append( [ast.literal_eval(i) for i in group["norm_xywh"].values] )
-        scores.append( group["score"].values )
-        classes.append( group["class"].values )
-        unified_classes.append( group["unifed_class"].values )
-        truncations.append( group.truncation.values )
-        occlustions.append( group.occlusion.values )
+        heigth.append(ast.literal_eval(group["shape"].values[0])[0])
+        width.append(ast.literal_eval(group["shape"].values[0])[1])
+        coco.append( [ast.literal_eval(x) for x in group["norm_coco"].values] )
+        classes.append( group["class"].values.tolist() )
+        unified_classes.append( group["unifed_class"].values.tolist())
     df = pd.DataFrame()
     df["video_name"] = video_name
     df["frame_index"] = frame_index
-    df["img_shape"] = img_shape
-    df["xywhs"] = xywhs
-    df["norm_xywhs"] = norm_xywhs
-    df["scores"] = scores
+    
+    img_path_list = []
+    for i in range(len(df)):
+        img_path_list.append(df["video_name"].values[i] + "/" +str(df["frame_index"].values[i]).zfill(7) +".jpg")
+    df["img_path"] = img_path_list
+
+    df["coco"] = coco
+    xyxy_list = []
+    yolo_list = []
+    for i in range(len(df)):
+        xyxy_list.append(coco2xyxy(df["coco"].values[i]))
+        yolo_list.append(coco2yolo(df["coco"].values[i]))
+    df["xyxy"] = xyxy_list
+    df["yolo"] = yolo_list
+
+    df["height"] = heigth
+    df["width"] = width
     df["classes"] = classes
     df["unified_classes"] = unified_classes
-    df["truncations"] = truncations
-    df["occlusion"] = occlustions
     return df
+
