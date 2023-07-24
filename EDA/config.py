eb5ba68951e5198bc09e33753cab7bcbd828c510
Ko Minsu <rh987201@gmail.com>
2023-07-24 13:11:55 +0900

    Create config.py
    이후 사용자의 하이퍼 파라미터를 모아둘 계획

---
+++ EDA/config.py
@@ -0,0 +1,56 @@
+import albumentations as A
+import cv2
+
+DATAPATH = "D:/visdrone/"
+IMAGE_SIZE = 416
+class_change_dict = {
+    1:0,# people
+    2:0,# pedestrian
+    4:1,# car
+    }   
+DATA_FORMAT = "pascal_voc"
+
+scale = 1.1
+train_transforms = A.Compose(
+    [
+        A.LongestMaxSize(max_size=int(IMAGE_SIZE * scale)),
+        A.PadIfNeeded(
+            min_height=int(IMAGE_SIZE * scale),
+            min_width=int(IMAGE_SIZE * scale),
+            border_mode=cv2.BORDER_CONSTANT,
+        ),
+        A.RandomCrop(width=IMAGE_SIZE, height=IMAGE_SIZE),
+        A.ColorJitter(brightness=0.6, contrast=0.6, saturation=0.6, hue=0.6, p=0.4),
+        A.OneOf(
+            [
+                A.ShiftScaleRotate(
+                    rotate_limit=20, p=0.5, border_mode=cv2.BORDER_CONSTANT
+                ),
+                A.IAAAffine(shear=15, p=0.5, mode="constant"),
+            ],
+            p=1.0,
+        ),
+        A.HorizontalFlip(p=0.5),
+        A.Blur(p=0.1),
+        A.CLAHE(p=0.1),
+        A.Posterize(p=0.1),
+        A.ToGray(p=0.1),
+        A.ChannelShuffle(p=0.05),
+    ],
+    bbox_params=A.BboxParams(format=DATA_FORMAT, min_visibility=0.4, label_fields=[],),
+)
+test_transforms = A.Compose(
+    [
+        A.LongestMaxSize(max_size=IMAGE_SIZE),
+        A.PadIfNeeded(
+            min_height=IMAGE_SIZE, min_width=IMAGE_SIZE, border_mode=cv2.BORDER_CONSTANT
+        ),
+    ],
+    bbox_params=A.BboxParams(format=DATA_FORMAT, min_visibility=0.4, label_fields=[]),
+)
+
+CUSTOM_CLASSES = [
+    "people",
+    "car",
+    "truck"
+]
