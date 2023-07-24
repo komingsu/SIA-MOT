c50fa8b9c01b1aa40d64663f0ddc770d09574c4b
Ko Minsu <rh987201@gmail.com>
2023-07-24 13:15:43 +0900

    baseline try2
    torch 이후 다시 tensorflow를 이용하여 시도, 훈련중간 nan 값이 계속해서 발생, 이유를 알수없음. 추측으로는 giou를 사용해서, albumentation의 augmentation에서 bbox가 사라지는 경우

---
+++ try2/core/config.py
@@ -0,0 +1,44 @@
+from easydict import EasyDict as edict
+import albumentations as A
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+__C                           = edict()
+
+cfg                           = __C
+
+# YOLO options
+__C.YOLO                      = edict()
+
+# Set the class name
+__C.YOLO.CLASSES              = "./data/classes/custom_visdrone.names"
+__C.YOLO.ANCHORS              = "./data/anchors/basline_anchors.txt"
+__C.YOLO.STRIDES              = [8, 16, 32]
+__C.YOLO.ANCHOR_PER_SCALE     = 3
+__C.YOLO.IOU_LOSS_THRESH      = 0.5
+
+# Train options
+__C.TRAIN                     = edict()
+
+__C.TRAIN.ANNOT_PATH          = "./data/dataset/visdrone_train.txt"
+__C.TRAIN.BATCH_SIZE          = 4
+# __C.TRAIN.INPUT_SIZE            = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608]
+__C.TRAIN.INPUT_SIZE          = [416]
+__C.TRAIN.DATA_AUG            = True
+__C.TRAIN.LR_INIT             = 1e-3
+__C.TRAIN.LR_END              = 1e-6
+__C.TRAIN.WARMUP_EPOCHS       = 0
+__C.TRAIN.EPOCHS              = 2
+
+
+# TEST options
+__C.TEST                      = edict()
+
+__C.TEST.ANNOT_PATH           = "./data/dataset/visdrone_test.txt"
+__C.TEST.BATCH_SIZE           = 2
+__C.TEST.INPUT_SIZE           = [416]
+__C.TEST.DATA_AUG             = False
+__C.TEST.DECTECTED_IMAGE_PATH = "./data/detection/"
+__C.TEST.SCORE_THRESHOLD      = 0.2
+__C.TEST.IOU_THRESHOLD        = 0.2
